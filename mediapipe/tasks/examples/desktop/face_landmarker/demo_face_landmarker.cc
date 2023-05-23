2ea09c24cc3c45a1ed6eb3f7def5bf66d385ce91
vain05 <truongduy20226@gmail.com>
2023-05-23 16:07:15 +0700

    add map blendshapes name to index

--- mediapipe/tasks/examples/desktop/face_landmarker/demo_face_landmarker.cc
+++ mediapipe/tasks/examples/desktop/face_landmarker/demo_face_landmarker.cc
@@ -1,25 +1,17 @@
 #include <cstdlib>
+#include <map>
 
 #include "absl/flags/flag.h"
 #include "absl/flags/parse.h"
 #include "mediapipe/framework/formats/image_frame.h"
-#include "mediapipe/framework/formats/image_frame_opencv.h"
 #include "mediapipe/framework/formats/image_opencv.h"
 #include "mediapipe/tasks/cc/vision/face_landmarker/face_landmarker.h"
-// #include "mediapipe/tasks/cc/vision/face_landmarker/face_blendshape_graph.h"
 #include "mediapipe/framework/port/opencv_highgui_inc.h"
 #include "mediapipe/framework/port/opencv_imgproc_inc.h"
 #include "mediapipe/framework/port/opencv_video_inc.h"
 #include "mediapipe/framework/port/status.h"
 
 constexpr char kWindowName[] = "Face Landmarker";
-
-ABSL_FLAG(std::string, input_video_path, "",
-          "Full path of video to load. "
-          "If not provided, attempt to use a webcam.");
-ABSL_FLAG(std::string, output_video_path, "",
-          "Full path of where to save result (.mp4 only). "
-          "If not provided, show result in a window.");
 
 static constexpr std::array<absl::string_view, 52> kBlendshapeNames = {
     "_neutral",
@@ -73,7 +65,75 @@ static constexpr std::array<absl::string_view, 52> kBlendshapeNames = {
     "mouthUpperUpLeft",
     "mouthUpperUpRight",
     "noseSneerLeft",
-    "noseSneerRight"};
+    "noseSneerRight"
+};
+
+static constexpr std::map<std::string, int> kBlendshapesNamesToIndex = {
+   { "_neutral" = 0 },
+   { "browDownLeft" = 1},
+   { "browDownRight" = 2},
+   { "browInnerUp" = 3},
+   { "browOuterUpLeft" = 4},
+   { "browOuterUpRight" = 5},
+   { "cheekPuff" = 6},
+   { "cheekSquintLeft" = 7},
+   { "cheekSquintRight" = 8},
+   { "eyeBlinkLeft" = 9},
+   { "eyeBlinkRight" = 10},
+   { "eyeLookDownLeft" = 11},
+   { "eyeLookDownRight" = 12},
+   { "eyeLookInLeft" = 13},
+   { "eyeLookInRight" = 14},
+   { "eyeLookOutLeft" = 15},
+   { "eyeLookOutRight" = 16},
+   { "eyeLookUpLeft" = 17},
+   { "eyeLookUpRight" = 18},
+   { "eyeSquintLeft" = 19},
+   { "eyeSquintRight" = 20},
+   { "eyeWideLeft" = 21},
+   { "eyeWideRight" = 22},
+   { "jawForward" = 23},
+   { "jawLeft" = 24},
+   { "jawOpen" = 25},
+   { "jawRight" = 26},
+   { "mouthClose" = 27},
+   { "mouthDimpleLeft" = 28},
+   { "mouthDimpleRight" = 29},
+   { "mouthFrownLeft" = 30},
+   { "mouthFrownRight" = 31},
+   { "mouthFunnel" = 32},
+   { "mouthLeft" = 33},
+   { "mouthLowerDownLeft" = 34},
+   { "mouthLowerDownRight" = 35},
+   { "mouthPressLeft" = 36},
+   { "mouthPressRight" = 37},
+   { "mouthPucker" = 38},
+   { "mouthRight" = 39},
+   { "mouthRollLower" = 40},
+   { "mouthRollUpper" = 41},
+   { "mouthShrugLower" = 42},
+   { "mouthShrugUpper" = 43},
+   { "mouthSmileLeft" = 44},
+   { "mouthSmileRight" = 45},
+   { "mouthStretchLeft" = 46},
+   { "mouthStretchRight" = 47},
+   { "mouthUpperUpLeft" = 48},
+   { "mouthUpperUpRight" = 49},
+   { "noseSneerLeft" = 50},
+   { "noseSneerRight" = 51}
+}
+
+
+ABSL_FLAG(std::string, input_video_path, "",
+          "Full path of video to load. "
+          "If not provided, attempt to use a webcam.");
+ABSL_FLAG(std::string, output_video_path, "",
+          "Full path of where to save result (.mp4 only). "
+          "If not provided, show result in a window.");
+
+ABSL_FLAG(std::vector<std::string>, blendshapes, "",
+          "List of the indices of blendshapes to show its prediction score. "
+          "If not provided, show nothing.")
 
 absl::Status RunFaceLandmarker()
 {
@@ -112,7 +172,6 @@ absl::Status RunFaceLandmarker()
         capture.set(cv::CAP_PROP_FPS, 30);
 #endif
     }
-
 
     LOG(INFO) << "Start grabbing and processing frames.";
     bool grab_frames = true;
@@ -192,7 +251,7 @@ absl::Status RunFaceLandmarker()
                     if (category.index == 25)
                     {
                         jaw_open_score = category.score;
-                        LOG(INFO) << kBlendshapeNames[category.index] << ' ' << category.score;
+                        // LOG(INFO) << kBlendshapeNames[category.index] << ' ' << category.score;
                     }
                 } 
             }
